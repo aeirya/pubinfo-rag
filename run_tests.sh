236df76d873ab87c9d4278d6c4c28db59eb137cc
aeirya <aeirya@gmail.com>
2026-05-18 14:19:24 +0200

    update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@
 python run.py \
     --query "for the article 'Exploring Dimensions of Trauma-Linked Somatic Complaints in Refugees: A Qualitative Study in Canada' the authors are: " \
     --template rag \
-    --limit 6 \
+    --limit 0 \
     --num-predict 10
     
