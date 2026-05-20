1228565126d7dcdc076d4f6217b878a4010fa040
aeirya <aeirya@gmail.com>
2026-05-20 20:29:59 +0200

    update question generation script

--- pubinfo/util/text.py
+++ pubinfo/util/text.py
@@ -13,3 +13,23 @@ def clean_abstract(text):
 
 def quote(s: str):
     return f'\"{s}\"'
+
+
+#### author
+
+from pandas import DataFrame
+
+def clean_authors(authors: str) -> list[str]:
+    authors = authors.replace("(Corresponding Author)", "")
+    authors = authors.replace("(Author)", "")
+    authors = authors.strip()
+    author_list = re.split(r"[;,]", authors)
+    # to remove weird spaces
+    author_list = [' '.join(a.split()) for a in author_list]
+    
+    return [a.strip() for a in author_list]
+
+def get_authors(data: DataFrame, row: int) -> list[str]:
+    authors = data["authors"][row]
+    return clean_authors(authors)
+
--- scripts/generate_questions.py
+++ scripts/generate_questions.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from pandas import DataFrame
 import random
+import argparse
+from argparse import Namespace
+from pubinfo.dataset import load_db
+from pubinfo.util.text import get_authors
 
 
-def pick_incorrect_authors(correct_author: str, data: pd.DataFrame):
+def pick_incorrect_authors(correct_author: str, data: DataFrame):
     """
     Input: The dataframe and correct author
     Output: 3 incorrect authors
@@ -13,8 +18,7 @@ def pick_incorrect_authors(correct_author: str, data: pd.DataFrame):
     while len(incorrect_authors) < 3:
         row = random.choice(range(len(data)))
 
-        authors = str(data["authors"][row])
-        authors = authors.split(";")
+        authors = get_authors(data, row)
 
         distractor = authors[random.choice(range(len(authors)))]
 
@@ -29,7 +33,18 @@ def pick_incorrect_authors(correct_author: str, data: pd.DataFrame):
     return incorrect_authors
 
 
-def pick_recent_articles(data: pd.DataFrame):
+def add_question(df: DataFrame, question: str, answers: dict[str, str], correct: str):
+    new_row = len(df)
+    df.loc[new_row] = {
+        "Question": question,
+        "A": answers["A"],
+        "B": answers["B"],
+        "C": answers["C"],
+        "D": answers["D"],
+        "Correct": correct,
+    }
+
+def pick_recent_articles(data: DataFrame):
     """
     Returns a list of 4 articles one of which is clearly the most recent one.
     This will be the first one.
@@ -55,76 +70,86 @@ def pick_recent_articles(data: pd.DataFrame):
     return list(articles)
 
 
-path = "publications/publications/kmanpub.csv"
-data = pd.read_csv(path)
+def insert_first_author_questions(data: DataFrame, multiple_choice: DataFrame):
+    # First author
+    for _ in range(20):
+        row = random.choice(range(len(data)))
 
-row = random.choice(range(len(data)))
+        title = data["title"][row]
+        authors = get_authors(data, row)
 
+        question = "Who is the first author of the following article: " + title
 
-multiple_choice = pd.DataFrame(
-    columns=["Question", "A", "B", "C", "D", "Correct"], dtype=str
-)
+        # Randomly decide which is going to be the correct anwser
+        # Always index 0, whatever lands there
+        options = ["A", "B", "C", "D"]
+        options = random.sample(options, 4)
+        correct = [options[0], authors[0]]
 
-# First author
-for _ in range(20):
-    row = random.choice(range(len(data)))
+        # Pick random incorrect answers and assign them to the remaining indices
+        incorrect_authors = pick_incorrect_authors(correct_author=correct[1], data=data)
+        distractor1 = [options[1], incorrect_authors[0]]
+        distractor2 = [options[2], incorrect_authors[1]]
+        distractor3 = [options[3], incorrect_authors[2]]
 
-    title = data["title"][row]
+        # write this information into the table
+        answers = {
+            correct[0]: correct[1],
+            distractor1[0]: distractor1[1],
+            distractor2[0]: distractor2[1],
+            distractor3[0]: distractor3[1],
+        }
 
-    authors = str(data["authors"][row])
-    authors = authors.split(";")
-
-    question = "Who is the first author of the following article: " + title
-
-    # Randomly decide which is going to be the correct anwser
-    # Always index 0, whatever lands there
-    options = ["A", "B", "C", "D"]
-    options = random.sample(options, 4)
-    correct = [options[0], authors[0]]
-
-    # Pick random incorrect answers and assign them to the remaining indices
-    incorrect_authors = pick_incorrect_authors(correct_author=correct[1], data=data)
-    distractor1 = [options[1], incorrect_authors[0]]
-    distractor2 = [options[2], incorrect_authors[1]]
-    distractor3 = [options[3], incorrect_authors[2]]
-
-    # write this information into the table
-    new_row = len(multiple_choice)
-    multiple_choice.loc[new_row] = str()
-    multiple_choice.loc[new_row, "Question"] = question
-    multiple_choice.loc[new_row, correct[0]] = correct[1]
-    multiple_choice.loc[new_row, distractor1[0]] = distractor1[1]
-    multiple_choice.loc[new_row, distractor2[0]] = distractor2[1]
-    multiple_choice.loc[new_row, distractor3[0]] = distractor3[1]
-    multiple_choice.loc[new_row, "Correct"] = correct[0]
+        add_question(multiple_choice, question, answers, correct[0])
 
 
-# Which of these articles is the most recent?
+def insert_most_recent_article_questions(data: DataFrame, multiple_choice: DataFrame):
+    # Which of these articles is the most recent?
+    for _ in range(20):
+        # The first one is the most recent one
+        titles = pick_recent_articles(data)
+        question = "Which of these articles is the most recent one?"
 
-for _ in range(20):
-    # The first one is the most recent one
-    titles = pick_recent_articles(data)
-    question = "Which of these articles is the most recent one?"
+        # Randomly decide which is going to be the correct anwser
+        # Always index 0, whatever lands there
+        options = ["A", "B", "C", "D"]
+        options = random.sample(options, 4)
+        correct = [options[0], titles[0]]
 
-    # Randomly decide which is going to be the correct anwser
-    # Always index 0, whatever lands there
-    options = ["A", "B", "C", "D"]
-    options = random.sample(options, 4)
-    correct = [options[0], titles[0]]
+        distractor1 = [options[1], titles[1]]
+        distractor2 = [options[2], titles[2]]
+        distractor3 = [options[3], titles[3]]
 
-    distractor1 = [options[1], titles[1]]
-    distractor2 = [options[2], titles[2]]
-    distractor3 = [options[3], titles[3]]
+        # write this information into the table
+        answers = {
+            correct[0]: correct[1],
+            distractor1[0]: distractor1[1],
+            distractor2[0]: distractor2[1],
+            distractor3[0]: distractor3[1],
+        }
+        add_question(multiple_choice, question, answers, correct[0])
+    
 
-    # write this information into the table
-    new_row = len(multiple_choice)
-    multiple_choice.loc[new_row] = str()
-    multiple_choice.loc[new_row, "Question"] = question
-    multiple_choice.loc[new_row, correct[0]] = correct[1]
-    multiple_choice.loc[new_row, distractor1[0]] = distractor1[1]
-    multiple_choice.loc[new_row, distractor2[0]] = distractor2[1]
-    multiple_choice.loc[new_row, distractor3[0]] = distractor3[1]
-    multiple_choice.loc[new_row, "Correct"] = correct[0]
+def main(args: Namespace):
+    data = load_db(args.dataset)
 
+    multiple_choice = pd.DataFrame(
+        columns=["Question", "A", "B", "C", "D", "Correct"], dtype=str
+    )
+    
+    insert_first_author_questions(data, multiple_choice)
+    insert_most_recent_article_questions(data, multiple_choice)
 
-multiple_choice.to_csv("multiple_coice.csv", index=False)
+    multiple_choice.to_csv(args.out, index=False)
+    
+
+def parse_args():
+    p = argparse.ArgumentParser(description="Generate QA Dataset")
+    p.add_argument("--dataset", default="kmanpub", help="data/publications file, without .csv or .jsonl")
+    p.add_argument("--out", default='multiple_coice.csv', help="output path")
+    return p.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
+
